1df29c2a87d103e7d386149d1a8088dbf5b1fbbb
jayjaehunchoi <wogns0108@naver.com>
2022-10-05 09:51:48 +0900

    feat: initial commit

---
+++ crew-service/src/test/java/com/huni/crewservice/CrewServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huni.crewservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrewServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
