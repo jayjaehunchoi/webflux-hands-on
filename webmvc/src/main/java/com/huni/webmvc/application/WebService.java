dce69a0b0375140e40b6d4205ce042eec43eba68
jayjaehunchoi <wogns0108@naver.com>
2022-10-06 10:16:28 +0900

    refactor: thread 개수를 수정

--- webmvc/src/main/java/com/huni/webmvc/application/WebService.java
+++ webmvc/src/main/java/com/huni/webmvc/application/WebService.java
@@ -2,16 +2,19 @@ package com.huni.webmvc.application;
 
 import com.huni.webmvc.presentation.CrewResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class WebService {
 
     private final RestTemplate restTemplate;
 
     public CrewResponse[] getAll() {
+        log.info("MVC : getAll");
         return restTemplate.getForEntity("/api/crews", CrewResponse[].class)
                 .getBody();
     }
